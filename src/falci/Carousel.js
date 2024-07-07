b5e9a2b7161353aa8a299f3ead04f85737784a6f
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-07-08 02:42:02 +0300

    fix: Updated card width in Parallax Carousel component to be consistent around edges

--- src/falci/Carousel.js
+++ src/falci/Carousel.js
@@ -7,13 +7,16 @@ import {
   Dimensions,
   Animated,
 } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+// import Ionicons from 'react-native-vector-icons/Ionicons'; // TODO icon problem will be fixed
 
 const {width: screenWidth} = Dimensions.get('window');
 
 const data = [
   {
     title: 'Kahve Falı',
-    description: 'Kahve mi içtiniz falınızı mı merak ediyorsunuz doğru yerdesiniz.',
+    description:
+      'Kahve mi içtiniz falınızı mı merak ediyorsunuz doğru yerdesiniz.',
     illustration: require('../assets/home-slider-background.png'),
   },
   {
@@ -23,7 +26,8 @@ const data = [
   },
   {
     title: 'El Falı',
-    description: 'El falınıza baktırmak için uzman falcılarımızla iletişime geçin.',
+    description:
+      'El falınıza baktırmak için uzman falcılarımızla iletişime geçin.',
     illustration: require('../assets/home-slider-background.png'),
   },
 ];
@@ -37,7 +41,7 @@ const ParallaxCarousel = () => {
     setTimeout(() => {
       if (scrollViewRef.current) {
         scrollViewRef.current.scrollTo({
-          x: screenWidth * 0.8 + 20,
+          x: (screenWidth * 0.7 + 12), // 1. indekse kaydır
           animated: true,
         });
       }
@@ -50,7 +54,7 @@ const ParallaxCarousel = () => {
         ref={scrollViewRef}
         horizontal
         showsHorizontalScrollIndicator={false}
-        snapToInterval={screenWidth * 0.8 + 20}
+        snapToInterval={screenWidth * 0.7 + 12} // Kart aralığı 20px
         decelerationRate="fast"
         contentContainerStyle={styles.scrollViewContent}
         onScroll={Animated.event(
@@ -60,9 +64,9 @@ const ParallaxCarousel = () => {
         scrollEventThrottle={16}>
         {data.map((item, index) => {
           const inputRange = [
-            (index - 1) * (screenWidth * 0.8 + 20),
-            index * (screenWidth * 0.8 + 20),
-            (index + 1) * (screenWidth * 0.8 + 20),
+            (index - 1) * (screenWidth * 0.7 + 12),
+            index * (screenWidth * 0.7 + 12),
+            (index + 1) * (screenWidth * 0.7 + 12),
           ];
 
           const translateX = scrollX.interpolate({
@@ -70,9 +74,15 @@ const ParallaxCarousel = () => {
             outputRange: [-50, 0, 50],
           });
 
+          const scale = scrollX.interpolate({
+            inputRange,
+            outputRange: [0.9, 1, 0.9],
+            extrapolate: 'clamp',
+          });
+
           return (
-            <View
-              style={{width: screenWidth * 0.8, marginHorizontal: 10}}
+            <Animated.View
+              style={[styles.cardContainer, {transform: [{scale}]}]}
               key={index}>
               <View style={styles.card}>
                 <Animated.Image
@@ -83,11 +93,24 @@ const ParallaxCarousel = () => {
                   <Text style={styles.title}>{item.title}</Text>
                   <Text style={styles.description}>{item.description}</Text>
                   <TouchableOpacity style={styles.button}>
-                    <Text style={styles.buttonText}>Hemen Baktır</Text>
+                    <LinearGradient
+                      colors={['#ffaf37', '#ff793f']}
+                      start={{x: 0, y: 0}}
+                      end={{x: 1, y: 1}}
+                      style={styles.gradient}>
+                      <View style={styles.buttonContent}>
+                        <Text style={styles.buttonText}>Hemen Baktır</Text>
+                        {/* <Ionicons // TODO icon problem will be fixed
+                          name="chevron-forward"
+                          size={16}
+                          color="#151522"
+                        /> */}
+                      </View>
+                    </LinearGradient>
                   </TouchableOpacity>
                 </View>
               </View>
-            </View>
+            </Animated.View>
           );
         })}
       </Animated.ScrollView>
@@ -103,10 +126,14 @@ const styles = StyleSheet.create({
   },
   scrollViewContent: {
     alignItems: 'center',
-    paddingHorizontal: (screenWidth - (screenWidth * 0.8 + 20)) / 2,
+    paddingHorizontal: (screenWidth - screenWidth * 0.7) / 2, // Kart aralığı 20px sağlamak için yarısını ekledik
+  },
+  cardContainer: {
+    width: screenWidth * 0.7,
+    marginHorizontal: 4, // Kart aralığı 4px
   },
   card: {
-    height: 360,
+    height: 300,
     borderRadius: 8,
     overflow: 'hidden',
     backgroundColor: '#f8f8f8',
@@ -124,30 +151,40 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     padding: 16,
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-    borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8,
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    fontSize: 16,
+    fontWeight: '500',
     color: '#fff',
   },
   description: {
-    fontSize: 14,
-    color: '#fff',
-    marginTop: 4,
+    fontSize: 12,
+    color: '#A3A3A3',
+    marginTop: 9,
+    lineHeight: 18,
+    fontWeight: '400',
   },
   button: {
-    backgroundColor: '#fbc02d',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 8,
-    marginTop: 10,
+    borderRadius: 4,
+    marginTop: 24,
+    width: 132,
+    height: 32,
+    overflow: 'hidden',
+  },
+  gradient: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 4,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   buttonText: {
-    color: '#000',
-    fontWeight: 'bold',
+    color: '#151522',
+    fontWeight: '500',
+    marginRight: 5,
   },
 });
 
