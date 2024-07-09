2843c797fe7e32eaa293bd7351e419fa9ddc352e
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-07-10 02:00:43 +0300

    feat: Updated import statements to include Login.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
-import Home from './src/screens/home';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
 
 const App = () => {
   return (
     <NativeBaseProvider>
       <Home />
+      {/* <Login /> */}
     </NativeBaseProvider>
   );
 };
