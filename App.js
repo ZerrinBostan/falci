a8b77c81e904281cffe4481347254db65df61a3f
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-07-09 04:01:05 +0300

    refactor: Move Home screen to a separate file and clean up App.js

--- App.js
+++ App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
 import {NativeBaseProvider} from 'native-base';
-import Header from './src/components/FortuneTeller/Homepage/Header';
-import Carousel from './src/components/FortuneTeller/Homepage/Carousel';
-import Featured from './src/components/FortuneTeller/Homepage/Featured';
-
-import DailyAstrology from './src/components/FortuneTeller/Homepage/DailyAstrology';
+import Home from './src/screens/home';
 
 const App = () => {
   return (
     <NativeBaseProvider>
-      <ScrollView style={styles.container}>
-        <Header />
-        <Carousel />
-        <Featured />
-        <DailyAstrology />
-      </ScrollView>
+      <Home />
     </NativeBaseProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
