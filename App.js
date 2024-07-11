590363bb7aa84511127e2d21b000ddec158e5ce3
Zerrin Tepedelen <zerrinbstn@gmail.com>
2024-07-11 11:01:47 +0300

    updated:  App.js to include the ForgotPassword screen in the navigation flow.

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import {NativeBaseProvider} from 'native-base';
-import Home from './src/screens/Home';
-import Login from './src/screens/Login';
+// import Login from './src/screens/Login';
+// import Home from './src/screens/Home'; 
+import ForgotPassword from './src/screens/ForgotPassword';
 
 const App = () => {
   return (
     <NativeBaseProvider>
-      <Home />
+      {/* <Home /> */}
       {/* <Login /> */}
+      {/* Home */}
+      <ForgotPassword />
     </NativeBaseProvider>
   );
 };
 
 export default App;
+//TODO: navigator stack error will be fixed
+
+/*
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+const Stack = createStackNavigator();
+
+<NativeBaseProvider>
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  </NavigationContainer>
+</NativeBaseProvider>
+*/
